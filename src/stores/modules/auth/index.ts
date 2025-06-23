6eea2db9fe05aed8bb288720e0bfb499f77fe7aa
夏季 <2937375029@qq.com>
2025-06-23 16:36:55 +0800

    feat(auth): 完善用户权限和菜单处理
    - 添加用户信息和登录状态管理
    - 引入菜单处理工具函数
    - 优化菜单列表计算逻辑，区分显示菜单和扁平化菜单
    - 为后续的权限控制和菜单动态加载做准备

--- src/stores/modules/auth/index.ts
+++ src/stores/modules/auth/index.ts
@@ -2,23 +2,30 @@ import { SetupStoreId } from '@/enum'
 import { fetchLogin, getAuthMenuListApi } from '@/service/api/login'
 import { defineStore } from 'pinia'
 import { useRoute, useRouter } from 'vue-router'
+import { getFlatMenuList, getShowMenuList } from './shared'
 
 export const useAuthStore = defineStore(SetupStoreId.Auth, () => {
   const router = useRouter()
   const route = useRoute()
 
+  // token
   const token = ref(window.localStorage.getItem('token'))
 
+  // 用户信息
   const userInfo: Api.Auth.UserInfo = reactive({
     userId: '',
     userName: '',
   })
 
+  // 是否已登录
   const isLogin = computed(() => Boolean(token.value))
 
+  // 菜单权限列表
   const menuList = ref<App.Global.Menu.MenuOptions[]>([])
-  const authMenuListGet = computed(() => menuList.value)
-  const flatMenuListGet = computed(() => menuList.value)
+  // 菜单权限列表 ==> 左侧菜单栏渲染，需要剔除 isHide == true
+  const authMenuListGet = computed(() => getShowMenuList(menuList.value))
+  // 菜单权限列表 ==> 扁平化之后的一维数组菜单，主要用来添加动态路由
+  const flatMenuListGet = computed(() => getFlatMenuList(menuList.value))
 
   const setToken = (value: string) => {
     token.value = value
