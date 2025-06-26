5c2815a37fabadd55e93ebbc73f87ed5262e7eae
夏季 <2937375029@qq.com>
2025-06-26 15:48:12 +0800

    refactor: 优化代码结构和样式
    - 移除 @unocss/reset 依赖
    - 更新 ESLint 配置和插件
    - 调整页面标题和元数据
    - 组织和优化导入语句
    - 调整主题设置和类型定义

--- src/plugins/iconify.ts
+++ src/plugins/iconify.ts
@@ -1,9 +1,8 @@
-import type { Env } from '@/typings/vite-env'
 import { addAPIProvider } from '@iconify/vue'
 
 /** Setup the iconify offline */
 export function setupIconifyOffline() {
-  const { VITE_ICONIFY_URL } = import.meta.env as Env.ImportMeta
+  const { VITE_ICONIFY_URL } = import.meta.env
 
   if (VITE_ICONIFY_URL) {
     addAPIProvider('', { resources: [VITE_ICONIFY_URL] })
