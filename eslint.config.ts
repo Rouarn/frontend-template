7ab477a6d7bf3b565f2988d5f3ae6e345739d0f9
夏季 <2937375029@qq.com>
2025-06-27 11:09:57 +0800

    build(dependencies): 添加页面路由和布局插件
    - 新增 vite-plugin-pages 和 vite-plugin-vue-layouts 插件
    - 更新 eslint 配置，增加文件忽略规则
    - 调整 package.json，添加新插件依赖

--- eslint.config.ts
+++ eslint.config.ts
@@ -9,6 +9,7 @@ export default defineConfigWithVueTs(
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
+
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   pluginVue.configs['flat/essential'],
