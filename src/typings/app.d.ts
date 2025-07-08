1d911bce441e43cadc2ef2c7c089659a7ebfc8d0
夏季 <2937375029@qq.com>
2025-07-08 17:21:58 +0800

    refactor: 移除未使用的 Menu 接口
    - 删除了 MenuOptions 和 MetaProps 接口
    - 清理了未使用的代码，提高了代码的可维护性

--- src/typings/app.d.ts
+++ src/typings/app.d.ts
@@ -146,28 +146,6 @@ declare namespace App {
     }
   }
 
-  /* Menu */
-  namespace Menu {
-    interface MenuOptions {
-      path: string
-      name: string
-      component?: string | (() => Promise<unknown>)
-      redirect?: string
-      meta: MetaProps
-      children?: MenuOptions[]
-    }
-    interface MetaProps {
-      icon: string
-      title: string
-      activeMenu?: string
-      isLink?: string
-      isHide: boolean
-      isFull: boolean
-      isAffix: boolean
-      isKeepAlive: boolean
-    }
-  }
-
   /** Service namespace */
   namespace Service {
     interface ServiceConfigItem {
