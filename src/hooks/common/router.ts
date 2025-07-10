38cdf7cb4e16ed4d3e5e3273345321bf92fefa00
夏季 <2937375029@qq.com>
2025-07-10 14:49:31 +0800

    feat(layouts): 更新 GlobalHeader 组件功能
    - 在 router.ts 中添加日志输出，便于调试
    - 在 GlobalHeader 组件中引入 useRouterPush 钩子
    - 优化 selectedKey 计算逻辑，移除冗余代码
    - 在 NMenu 组件中添加 @update:value 事件处理，实现路由跳转

--- src/hooks/common/router.ts
+++ src/hooks/common/router.ts
@@ -37,6 +37,7 @@ export function useRouterPush(inSetup = true) {
   }
 
   function routerPushByKeyWithMetaQuery(key: App.Global.RouteKey) {
+    console.log('key: ', key)
     const allRoutes = router.getRoutes()
     const meta = allRoutes.find((item) => item.name === key)?.meta || null
 
--- src/layouts/modules/global-header/index.vue
+++ src/layouts/modules/global-header/index.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 import { useRouteStore } from '@/stores/modules/route'
+import { useRouterPush } from '@/hooks/common/router'
 
 defineOptions({
   name: 'GlobalHeader',
 })
 
-const ruterStore = useRouteStore()
 const route = useRoute()
+const ruterStore = useRouteStore()
+const { routerPushByKeyWithMetaQuery } = useRouterPush()
 
 const selectedKey = computed(() => {
   const { hideInMenu, activeMenu } = route.meta
@@ -17,11 +19,10 @@ const selectedKey = computed(() => {
 
   return routeName
 })
-console.log('selectedKey: ', selectedKey)
 </script>
 
 <template>
-  <NMenu :value="selectedKey" mode="horizontal" :options="ruterStore.menus" responsive />
+  <NMenu :value="selectedKey" mode="horizontal" :options="ruterStore.menus" responsive @update:value="routerPushByKeyWithMetaQuery" />
 </template>
 
 <style lang="scss" scoped></style>
