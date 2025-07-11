7dc13f196356d10aaf602b86ab4179c156bcf782
夏季 <2937375029@qq.com>
2025-07-11 13:37:11 +0800

    docs(i18n): 更新中英文本的描述
    - 在英文和中文文件中将 "后台管理模板" 修改为 "前端开发模板"
    - 在英文文件中将 "front-end" 修改为 "Frontend" 以保持一致性

--- src/locales/langs/en-us.ts
+++ src/locales/langs/en-us.ts
@@ -12,7 +12,7 @@ const local: App.I18n.Schema = {
   page: {
     about: {
       title: 'About',
-      introduction: `FrontendTemplate is an elegant and powerful back-end management template based on the latest front-end technology stack, including Vue3, Vite7, TypeScript, Pinia and UnoCSS. It is equipped with a rich set of theme configurations and components, and its code is standardized and rigorous, achieving an automated file routing system. We offer you a one-stop front-end development solution that requires no additional configuration and is ready to use out of the box. It is also a best practice for quickly learning cutting-edge technologies.`,
+      introduction: `FrontendTemplate is an elegant and powerful front-end Development template based on the latest front-end technology stack, including Vue3, Vite7, TypeScript, Pinia and UnoCSS. It is equipped with a rich set of theme configurations and components, and its code is standardized and rigorous, achieving an automated file routing system. We offer you a one-stop front-end development solution that requires no additional configuration and is ready to use out of the box. It is also a best practice for quickly learning cutting-edge technologies.`,
       projectInfo: {
         title: 'Project Info',
         version: 'Version',
